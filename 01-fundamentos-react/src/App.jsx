8aaf18267dfb122af602c68ab5ae69fd324748dc
rafael <rafaelvrcr@gmail.com>
2024-07-02 15:09:42 -0300

    Iniciando com React aula 05 - Entendendo componentes e sua importação

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,8 +1,11 @@
-function App() {
+import { Post } from "./Post";
+
+export function App() {
 
   return (
-   <h1>My first React App</h1>
+    <>
+      <h1>My First React App</h1>
+      <Post/>    
+    </>
   )
 }
-
-export default App
