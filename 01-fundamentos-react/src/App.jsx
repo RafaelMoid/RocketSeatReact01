e7d2491d91d8e3af85cfb34d0782c7f8e53041a1
rafael <rafaelvrcr@gmail.com>
2024-07-03 16:40:54 -0300

    Iniciando com React aula 07 - CSS Modules

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,9 +1,11 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
 
 export function App() {
 
   return (
     <>
+      <Header />
       <h1>My First React App</h1>
       <Post 
         deck="Burakos"
