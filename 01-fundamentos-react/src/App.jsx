0d458913220774d2e0d26ee406fe38081c3d80a4
rafael <rafaelvrcr@gmail.com>
2024-07-03 16:19:26 -0300

    Iniciando com React aula 06 - Entendendo Propriedades/Props

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -5,7 +5,14 @@ export function App() {
   return (
     <>
       <h1>My First React App</h1>
-      <Post/>    
+      <Post 
+        deck="Burakos"
+        colors="UB"
+      />    
+      <Post
+        deck="Zimone and Dina"
+        colors="UBG"
+      />    
     </>
   )
 }
--- 01-fundamentos-react/src/Post.jsx
+++ 01-fundamentos-react/src/Post.jsx
@@ -1,3 +1,8 @@
-export function Post() {
-    return <p>Post</p>
+export function Post(props) {
+    return (
+        <>
+            <h2>{props.deck}</h2>
+            <h3>{props.colors}</h3>
+        </>
+    )
 }
