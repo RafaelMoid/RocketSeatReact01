392b0b7791b9f346c2caf7092e1272737795b271
rafael <rafaelvrcr@gmail.com>
2024-07-05 15:24:55 -0300

    Aula finalizando Sidebar

--- 01-fundamentos-react/src/components/Sidebar.jsx
+++ 01-fundamentos-react/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from "./Sidebar.module.css";
 
 import backgroundImage from "../img/magic-card.jpg";
@@ -8,10 +10,11 @@ export function Sidebar() {
       <img src={backgroundImage} className={styles.backImage}/>
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/76971178?v=4"/>
         <strong>Rafael Varela</strong>
         <p>Code master</p>
         <footer>
-          <a href="#">Editar seu perfil</a>
+          <a href="#"><PencilLine size="20"/>Editar seu perfil</a>
         </footer>
       </div>
     </aside>
